422cd8f3f23a3b8f3787abdc8cbf1e95492e464e
mustafab11 <mustafab11@yahoo.com>
2020-05-06 18:42:18 +0300

    update Spring.java

--- src/tests/Spring.java
+++ src/tests/Spring.java
@@ -4,22 +4,7 @@ public class Spring {
 
     public static void main(String[] args) {
 
-
-        System.out.println("days breathing the spring");
-
-        System.out.println("days breathing the spring");
-
-        System.out.println("days breathing the spring");
-
-
-        System.out.println("days breathing the spring");
-        System.out.println("days breathing the spring");
-
-
-
-
-
-        System.out.println("days breathing the spring");
+   System.out.println("days breathing the spring");
 
         System.out.println("days breathing the spring");
 
