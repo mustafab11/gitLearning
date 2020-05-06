d491c18f7ad30245bf3b4b2667526cffbccbab25
mustafab11 <mustafab11@yahoo.com>
2020-05-06 17:45:49 +0300

    bm

--- src/tests/HelloGitWorld.java
+++ src/tests/HelloGitWorld.java
@@ -2,7 +2,7 @@ package tests;
 
 public class HelloGitWorld {
     public static void main(String[] args) {
-        System.out.println("Hello new World aain");
+        System.out.println("Hello new World aain, hello hello");
 
 
     }
--- src/tests/Login.java
+++ src/tests/Login.java
@@ -3,7 +3,7 @@ package tests;
 public class Login {
     public static void main(String[] args) {
 
-        System.out.println("This is the very beginning of git login");
+        System.out.println("hey Bilal, This is the very beginning of git login");
 
 
     }
